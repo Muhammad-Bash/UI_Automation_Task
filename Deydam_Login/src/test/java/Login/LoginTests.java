3f521557158deae3c284da2b5bde8ff2c2868850
Muhammad.Basheer.Ishola <muhammadbasheerishola@gmail.com>
2020-12-07 11:27:20 +0100

    Allure-Test-Results

--- Deydam_Login/src/test/java/Login/LoginTests.java
+++ Deydam_Login/src/test/java/Login/LoginTests.java
@@ -3,8 +3,10 @@ package Login;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -30,14 +32,52 @@ public class LoginTests {
 
         driver.findElement(By.id("password")).sendKeys("Ishola92#");
 
-        driver.findElement(By.xpath("/html/body/div/div/div/div/main/div/div[2]/div/div/div/div[2]/div/div/form/button")).click();}
+        driver.findElement(By.xpath("/html/body/div/div/div/div/main/div/div[2]/div/div/div/div[2]/div/div/form/button")).click();
+        Thread.sleep(1000);
+
+    }
+
+    @Test(priority = 0)
+    public void testSuccessfulLogin(){
+        if (driver.getCurrentUrl ().contains ("https://dev.d2rxvhrryr2bkn.amplifyapp.com/app/feed"))
+            //Pass
+            System.out.println ("The Page URL contains /app/feed");
+        else
+            //Fail
+            System.out.println ("The Page URL does not contain /app/feed");
+    }
+    @Test(priority = 1)
+    public void testLogout() throws InterruptedException {
+        //click on the profile button
+        driver.findElement (By.xpath ("//*[@id=\"root\"]/div/div/div/main/div/div[1]/div[1]/div/div[2]/div[3]/button")).click ();
+        Thread.sleep (2000);
+        //click on the logout button
+        driver.findElement (By.xpath ("//*[@id=\"root\"]/div/div/div/main/div/div[1]/div[1]/div/div[2]/div[3]/div/div/a[3]/p")).click ();
+        //printout response based on status
+        if(driver.getCurrentUrl ().contains ("https://dev.d2rxvhrryr2bkn.amplifyapp.com/login\""))
+            //Pass
+            System.out.println ("The Login page URL contains /login");
+        else
+            //Fail
+            System.out.println ("The Login URL does not contain /login");
+    }
+    @Test(priority = 1)
+    public void testNegativeLogin(){
+        driver.findElement (By.id ("username")).sendKeys ("invalidUsername");
+        driver.findElement (By.id ("password")).sendKeys ("admin123.");
+        driver.findElement (By.xpath ("//*[@id=\"root\"]/div/div/div/main/div/div[2]/div/div/div/div[2]/div/div/form/button")).click ();
+        String expectedErrorMessage = "Login Failed: Invalid username or password";
+        String actualErrorMessage = driver.findElement(By.xpath ("//*[@id=\"root\"]/div/div/div/main/div/div[2]/div/div/div/div[2]/div/div/form/div[4]/div/div/p")).getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
 
         public static void main(String[] args) throws InterruptedException{
         LoginTests test = new LoginTests();
         test.setUp();}
 
         @AfterClass
-            public void tearDown() {
+        public void closeBrowser () throws InterruptedException {
+            Thread.sleep(10000);
             driver.quit();
-        }
+    }
 }
--- Facebook_SignIn_2/src/test/java/SignIn/SignInTests.java
+++ Facebook_SignIn_2/src/test/java/SignIn/SignInTests.java
@@ -3,8 +3,10 @@ package SignIn;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 import java.util.concurrent.TimeUnit;
 
@@ -33,15 +35,51 @@ public class SignInTests {
 
             //locate and inspect the SignIn button
             driver.findElement(By.xpath("//*[@id=\"loginbutton\"]")).click();
-            Thread.sleep(20000);}
+            Thread.sleep(20000);
+        }
 
+
+    @Test(priority = 0)
+    public void testSuccessfulLogin(){
+        if (driver.getCurrentUrl ().contains ("https://www.facebook.com/login"))
+            //Pass
+            System.out.println ("The Page URL contains /app/feed");
+        else
+            //Fail
+            System.out.println ("The Page URL does not contain /app/feed");
+    }
+    @Test(priority = 1)
+    public void testLogout() throws InterruptedException {
+        //click on the profile button
+        driver.findElement (By.xpath ("//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[2]/div[4]/div[1]/span/div/div[1]")).click();
+        Thread.sleep (2000);
+        //click on the logout button
+        driver.findElement (By.xpath ("//*[@id=\"mount_0_0\"]/div/div[1]/div[1]/div[2]/div[4]/div[2]/div/div[2]/div[1]/div[1]/div/div/div/div/div/div/div/div/div[1]/div/div[3]/div/div[4]/div/div[1]/div[2]/div/div/div/div/span")).click ();
+        //printout response based on status
+        if(driver.getCurrentUrl ().contains ("https://www.facebook.com/login"))
+            //Pass
+            System.out.println ("The Login page URL contains /login");
+        else
+            //Fail
+            System.out.println ("The Login URL does not contain /login");
+    }
+    @Test(priority = 1)
+    public void testNegativeLogin(){
+        driver.findElement (By.id ("username")).sendKeys ("biz@33445");
+        driver.findElement (By.id ("password")).sendKeys ("admin123.");
+        driver.findElement (By.xpath ("//*[@id=\"u_0_f\"]")).click ();
+        String expectedErrorMessage = "The email address or phone number that you've entered doesn't match any account. Sign up for an account.";
+        String actualErrorMessage = driver.findElement(By.xpath ("//*[@id=\"email_container\"]/div[2]")).getText();
+        Assert.assertEquals(actualErrorMessage, expectedErrorMessage);
+    }
         public static void main(String args []) throws InterruptedException {
             SignIn.SignInTests test = new SignIn.SignInTests();
-            test.setUp(); }
+            test.setUp();
+        }
 
         @AfterClass
-        public void tearDown() {
-        driver.quit();
-    }
+        public void closeBrowser () throws InterruptedException {
+            Thread.sleep(10000);
+        driver.quit(); }
 
 }
